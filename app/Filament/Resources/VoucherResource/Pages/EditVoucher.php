31be545f928b9f6b6a6eb7247474c84b86a41943
anugrahp13 <anugrahprastyo13@gmail.com>
2025-05-27 23:50:39 +0700

    feat[EditVoucher.php]: Create page voucher

---
+++ app/Filament/Resources/VoucherResource/Pages/EditVoucher.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Filament\Resources\VoucherResource\Pages;
+
+use App\Filament\Resources\VoucherResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditVoucher extends EditRecord
+{
+    protected static string $resource = VoucherResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return VoucherResource::getUrl('index');
+    }
+}
