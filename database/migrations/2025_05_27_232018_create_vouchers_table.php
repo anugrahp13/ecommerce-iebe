bfef295bb601f6896cd03bdcbc98b494a88fc157
anugrahp13 <anugrahprastyo13@gmail.com>
2025-05-27 23:53:00 +0700

    feat[migration]: Create migration voucher

---
+++ database/migrations/2025_05_27_232018_create_vouchers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vouchers', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->unique();
+            $table->unsignedInteger('value'); // nominal voucher
+            $table->unsignedInteger('min_purchase')->default(0);
+            $table->unsignedInteger('quota')->default(1);
+            $table->date('start_at');
+            $table->date('end_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vouchers');
+    }
+};
