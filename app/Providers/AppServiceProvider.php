3cac9124876212548cf0a02ad44b061ef0641576
anugrahp13 <anugrahprastyo13@gmail.com>
2025-06-04 14:24:41 +0700

    refactor[AppServiceProvicder.php]: Added model and observer discount

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,10 +4,12 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use App\Models\Category;
+use App\Models\Discount;
 use App\Models\Product;
 use App\Models\User;
 use App\Models\Voucher;
 use App\Observers\CategoryObserver;
+use App\Observers\DiscountObserver;
 use App\Observers\ProductObserver;
 use App\Observers\UserObserver;
 use App\Observers\VoucherObserver;
@@ -31,6 +33,7 @@ class AppServiceProvider extends ServiceProvider
         User::observe(UserObserver::class);
         Voucher::observe(VoucherObserver::class);
         Product::observe(ProductObserver::class);
+        Discount::observe(DiscountObserver::class);
 
     }
 }
