25aaa6d3924bfcc3beacdf421904e11cd014a736
anugrahp13 <anugrahprastyo13@gmail.com>
2025-06-04 14:19:05 +0700

    refactor[CategoryResource.php]: Changed icon category

--- app/Filament/Resources/CategoryResource.php
+++ app/Filament/Resources/CategoryResource.php
@@ -24,7 +24,7 @@ class CategoryResource extends Resource
 {
     protected static ?string $model = Category::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-tag';
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
     protected static ?string $navigationLabel = 'Kategori';
     protected static ?string $navigationGroup = 'Manajemen';
 
